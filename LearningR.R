b8defb78c3ce9b13ef362afa66714a648426c8a2
Fabienne Ishimwe <ffabienne.iishimwe@gmail.com>
2017-09-29 09:41:46 -0400

    Add files via upload

--- LearningR.R
+++ LearningR.R
@@ -1,7 +1,9 @@
 
 
+
+
+
 #1. Matrix
-#===================================================================================================
 A = matrix( 
   c(2, 4, 3, 1, 5, 7), # the data elements 
   nrow=2,              # number of rows 
@@ -29,7 +31,6 @@ c(A)
 
 
 #2. Lists
-#===================================================================================================
 
 a= c(2, 3, 5) 
 b = c("aa", "bb", "cc", "dd", "ee") 
@@ -44,6 +45,7 @@ x[1]
 # Naming Lists
 
 y = list(mary=c(1991, 3, 5), dan=c("aa", "bb")) 
+y
 y['mary']
 y$dan
 y[['dan']][1]="new"
@@ -51,9 +53,7 @@ y$dan
 
 
 
-#3. DataFrames
-#===================================================================================================
-
+#DataFrames
 mtcars #built it dataframe
 
 head(mtcars) # preview of the data, first 6 rows
@@ -68,14 +68,9 @@ mtcars[[3]][mtcars$am == 0]
 
 
 
-#STATS WITH  R
-#===================================================================================================
-#===================================================================================================
-
+#Stats with R
 
 #1.QUALITATIVE DATA
-#===================================================================================================
-
 
 # Data used : painters built in dataframe
 library(MASS)
@@ -130,7 +125,6 @@ cbind(Mean_of_all_schools)
 
 
 #2. Quantitative Data
-#===================================================================================================
 
 #Data used : Built in Dataframe faithful
 
@@ -145,24 +139,27 @@ str(faithful)
 
 
 # Finding the frequency, relative and cumulative frequency distribution of the eruption durations in faithful.
+#1.Find the range
+duration=faithful$eruptions
+range(duration)
 
-              #1.Find the range
-              duration=faithful$eruptions
-              range(duration)
-              
-              #2. Breaking the range into non -overlapping intervals
-              breaks = seq(1.5, 5.5, by=0.5)    # half-integer sequence 
-              breaks
-              
-              #3. classify the data into the intervals
-              duration.cut = cut(duration, breaks, right=FALSE)
-              duration.cut
-              
-              duration.freq=table(duration.cut)
-              duration.Relfreq=table(duration.cut)/nrow(faithful)
-              duration.cumfreq=cumsum(duration.freq)
-              
-              cbind(duration.freq,duration.Relfreq,duration.cumfreq)
+#2. Breaking the range into non -overlapping intervals
+breaks = seq(1.5, 5.5, by=0.5)    # half-integer sequence 
+breaks
+
+#3. classify the data into the intervals
+duration.cut = cut(duration, breaks, right=FALSE)
+duration.cut
+
+duration.freq=table(duration.cut)
+duration.Relfreq=table(duration.cut)/nrow(faithful)
+duration.cumfreq=cumsum(duration.freq)
+
+cbind(duration.freq,duration.Relfreq,duration.cumfreq)
+
+
+
+
 
 
 
@@ -178,7 +175,6 @@ hist(duration,    # apply the hist function
 
 
 # cumulative frequency graph
-
 cumfreq0 = c(0, cumsum(duration.freq)) 
 plot(breaks, cumfreq0,            # plot the data 
      main="Faithful Eruptions",  # main title 
@@ -206,7 +202,6 @@ abline(lm(waiting ~ duration))
 
 
 #3.Numerical Mesures
-#===================================================================================================
 
 #Mean
 mean(duration)
@@ -271,6 +266,142 @@ skewness(duration)                # apply the skewness function
 # Kurtosis
 kurtosis(duration)                # apply the kurtosis function
 hist(duration)
+
+
+
+#4. Probability Distributions
+#===================================================================================================
+
+#4.1 Binomial Distribuition
+
+#example: Probaility of getting 4 questionbs correct on a 12 multiplte choice quiz. Only one answer can be correct out of 5 other choices
+dbinom(4, size=12, prob=0.2)  #exactly 4 questions,  probility of getting one correct =1/5=0.2
+pbinom(4, size=12, prob=0.2)  # 4 or less questions by random
+
+
+
+#4.2 Poisson Distribution
+# example : If there are twelve cars crossing a bridge per minute on average, the probability of having seventeen or more cars crossing the bridge in a particular minute is:
+ppois(16, lambda=12)
+
+
+
+
+#4.3 Contionuous uniform  Distribution
+#example : Selecting ten random numbers between one and three
+cbind(runif(10, min=1, max=3) )
+
+
+
+
+
+#Exponential Distribution
+#example :Suppose the mean checkout time of a supermarket cashier is three minutes. Find the probability of a customer checkout being completed by the cashier in less than two minutes.
+
+pexp(2, rate=1/3) 
+
+
+
+
+
+
+# normal distribution
+
+# example : Assume that the test scores of a college entrance exam fits a normal distribution. Furthermore, the mean test score is 72, and the standard deviation is 15.2. What is the percentage of students scoring 84 or more in the exam?
+
+pnorm(84, mean=72, sd=15.2, lower.tail=FALSE) 
+
+
+
+
+#5. simple linear regression
+#=================================================
+
+# Values of height
+x= c(151, 174, 138, 186, 128, 136, 179, 163, 152, 131)
+
+# Values of weight.
+y=c(63, 81, 56, 91, 47, 57, 76, 72, 62, 48)
+
+
+rel=lm(y~x)
+summary(rel)
+
+
+# Find weight of a person with height 170.
+a = data.frame(x=170)
+result = predict(rel,a)
+print(result)
+
+# Give the chart file a name.
+#png(file = "linearregression.png")
+
+# Plot the chart.
+plot(y,x,col = "blue",main = "Height & Weight Regression",
+     abline(lm(x~y)),cex = 1.3,pch = 16,xlab = "Weight in Kg",ylab = "Height in cm")
+
+# Save the file.
+#dev.off()
+
+
+
+
+#.6 Multiple linear regression
+#===================================================================================
+
+input <- mtcars[,c("mpg","disp","hp","wt")]
+input
+# Create the relationship model.
+model <- lm(mpg~disp+hp+wt, data = input)
+
+a <- coef(model)[1]
+Xdisp <- coef(model)[2]
+Xhp <- coef(model)[3]
+Xwt <- coef(model)[4]
+
+print(Xdisp)
+print(Xhp)
+print(Xwt)
+print(a)
+
+
+# Sample 
+Y = 37.15+(-0.000937)*221+(-0.0311)*102+(-3.8008)*2.91
+
+Y
+
+
+
+#.7 Logistics regression
+#===================================================================================
+input <- mtcars[,c("am","cyl","hp","wt")]
+input
+
+am.data = glm(formula = am ~ cyl + hp + wt, data = input, family = binomial)
+
+print(summary(am.data))
+
+
+
+#8. Decision Tree
+#===================================================================================
+#install.packages("party") # Package used to create Decision Trees
+
+# Load the party package. It will automatically load other dependent packages.
+#library(party)
+
+# Print some records from data set readingSkills.
+print(head(readingSkills))
+
+
+input.dat <- readingSkills[c(1:105),]
+input.dat
+# Create the tree.
+output.tree <- ctree(
+  nativeSpeaker ~ age + shoeSize + score, 
+  data = input.dat)
+output.tree
+plot(output.tree)
 
 
 
